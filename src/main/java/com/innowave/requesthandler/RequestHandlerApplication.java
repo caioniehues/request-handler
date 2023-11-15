2da0e6bd85a72c2311dee5211c2b3700fe5a7ebb
Caio Niehues <caio.niehues@innowave.tech>
2023-11-15 12:43:45 +0000

    Initial commit

---
+++ src/main/java/com/innowave/requesthandler/RequestHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.innowave.requesthandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestHandlerApplication.class, args);
+	}
+
+}
